40a7d9a48edc5409c5d138678f292b5f68a05431
Ryoichi Kato <solar.pandahero.96@gmail.com>
2019-06-26 20:43:04 -0700

    upgraded img jquery html

---
+++ jQuery.js
@@ -0,0 +1,10 @@
+$(function(){
+  $('.titlelogo').hover(){
+    $('#title-kaicho').hide();
+    $('#kanegon').fadeIn();
+  },
+  {
+    $('#title-kaicho').show();
+    $('#kanegon').fadeOut();
+  }
+})
