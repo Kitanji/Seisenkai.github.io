eb4ebb65dc5116284ffa084e5f71a4d4d7d187e6
Ryoichi Kato <solar.pandahero.96@gmail.com>
2019-06-27 22:23:01 -0700

    fixed

--- jQuery.js
+++ jQuery.js
@@ -1,24 +1,31 @@
-function animation(){
-  $('#SHINSHI').animate({opacity:1;},500,'liner').promise()
-  .then(function(){
-    return $('#ALmighty').animate({opacity:1;},500,'liner');
-  }).then(function(){
-    return $('#Leader').animate({opacity:1;},500,'liner');
-  }).then(function(){
-    return $('#head').animate({opacity:1;},300,'swing');
-  }).then(function(){
-    return $('.remark').animate({visibility:visible;});
-  }).then(function(){
-    return $('.mainSub').animate({visibility:visible;});
-  }).then(function(){
-    return $('.footTitle').animate({visibility:visible;});
-  }).always(function(){
+//function animation(){
+  //$('#SHINSHI').animate({opacity:1;},500,'liner').promise()
+  //.then(function(){
+  //  return $('#ALmighty').animate({opacity:1;},500,'liner');
+  //}).then(function(){
+    //return $('#Leader').animate({opacity:1;},500,'liner');
+  //}).then(function(){
+    //return $('#head').animate({opacity:1;},300,'swing');
+  //}).then(function(){
+    //return $('.remark').animate({visibility:visible;});
+  //}).then(function(){
+    //return $('.mainSub').animate({visibility:visible;});
+  //}).then(function(){
+    //return $('.footTitle').animate({visibility:visible;});
+  //}).always(function(){
 
-  });
-}
+  //});
+//}
 
 $(function(){
-  animation();
+  //animation();
+  $('#SHINSHI').animate({opacity:1;},500,'liner');
+  $('#ALmighty').animate({opacity:1;},500,'liner');
+  $('#Leader').animate({opacity:1;},500,'liner');
+  $('#head').animate({opacity:1;},300,'swing');
+  $('.remark').animate({visibility:visible;});
+  $('.mainSub').animate({visibility:visible;});
+  $('.footTitle').animate({visibility:visible;});
 
   $('.headTitle').hover(
     function(){
