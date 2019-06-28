fc12a4eb27a9945a14c67d66bc6bca2ce184216d
Ryoichi Kato <solar.pandahero.96@gmail.com>
2019-06-27 19:32:19 -0700

    updated

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Seisenkai.github.io
+
+facebook => リンク形式にする（サイト管理しやすい）
+https://www.facebook.com/Seisenkai/?view_public_for=2327999857443435
+
+本郷学園応援委員会のリンクを掲載する
+
+なんかオープニングアニメーションを作成する（2s)
+
+ページトップにスクロールする機能を搭載する
+
+問い合わせフォーム（Googleアドレスの収集)
--- jQuery.js
+++ jQuery.js
@@ -1,4 +1,28 @@
 $(function(){
+  var d = new $.Deferred();
+  d.promise()
+  .then(animation01())
+  .then(animation02())
+  .then(animation03());
+});
+
+function animation01(){
+  $('#body').hide();
+  d.resolve();
+}
+function animation02(){
+  $('.mainTitles').fadeIn(1000);
+}
+function animation03(){
+  $('.headTitle').fadeIn(500);
+}
+function animation04(){
+  $('.remark').show();
+  $('.mainSub').show();
+  $('.footTitle').show();
+}
+
+$(function(){
   $('.headTitle').hover(
     function(){
       $('#title-kaicho').hide();
